1dc3e3533cad4db7367d7fbfe835ba81afc4503e
GillesGMS <gillesgms0@gmail.com>
2025-04-29 16:32:06 -0300

    Add files via upload

---
+++ quizpaís.py
@@ -0,0 +1,11 @@
+user = input("Quer iniciar o quiz?")
+if  user.lower() == "sim":
+    print("Certo, vamos começar!")
+    resposta = input("Qual o país que foi responsável por invadir a URSS?")
+    if resposta == "Alemanha" or resposta == "Finlândia":
+        print("Boa, você acertou!")
+    else:
+        print("Errado!")
+
+elif user.lower() == "nao":
+    print("Ok, encerrando programa!")
