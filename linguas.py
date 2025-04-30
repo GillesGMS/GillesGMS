f26b740065e0bcf1325e97d7e3ce1ecad31d3384
GillesGMS <gillesgms0@gmail.com>
2025-04-30 12:52:27 -0300

    Add files via upload

---
+++ linguas.py
@@ -0,0 +1,6 @@
+user = input("Escolha uma lingua!")
+if user == "Português":
+    print("Este são os países que falam o Português: Portugal, Brasil, Angola, Cabo Verde, Moçambique, Timor-Leste")
+else:
+    if user == "Inglês":
+        print("Este são os países que falam o Inglês: EUA, Reino Unido, Canadá, Australia, Irlanda, Nova Zelândia")
