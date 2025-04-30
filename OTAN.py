21744251d1a0e4a514c1980bab91436b1422b1c4
GillesGMS <gillesgms0@gmail.com>
2025-04-30 12:37:47 -0300

    Add files via upload

---
+++ OTAN.py
@@ -0,0 +1,8 @@
+user = input("Selecione um país.")
+
+
+if user == "Estados Unidos" or user == "Canadá" or  user == "Albânia" or user == "Alemanha" or user == "Bélgica" or user == "Bulgária" or user == "Croácia" or user == "Dinamarca" or user == "Eslováquia" or user == "Eslovênia" or user == "Espanha" or user == "Estônia" or user == "Finlândia" or user == "França" or user == "Grécia" or user == "Hungria" or user == "Islândia" or user == "Itália" or user == "Letônia" or user == "Lituânia" or user == "Luxemburgo" or user == "Macedônia do Norte" or user == "Montenegro" or user == "Noruega" or user == "Países Baixos" or user == "Polônia" or user == "Portugal" or user == "República Checa" or user == "Romênia" or user == "Reino Unido":
+
+    print("Este é um membro da OTAN!") 
+else:
+    print("Este país não faz parte da OTAN!")
